4bdff98ec88c344a5ac48b4b36c557c512e78230
Andres Simoneau <andy20268@gmail.com>
2026-01-03 01:05:59 -0500

    s

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SQL-skyrim-weapons
+
+https://www.kaggle.com/datasets/elmartini/skyrim-weapons-dataset?resource=download
+
--- skyrim.py
+++ skyrim.py
@@ -1,1 +1,6 @@
+import pandas as pd 
+import sqlite3
 
+weapons = pd.read_csv('Skyrim_Weapons.csv')
+weapons_data = sqlite3.connect('skyrim.db')
+weapons.to_sql("weapons", weapons_data, if_exists="replace", index=False)
