c5fcca381c29f19604d914b77e8b2810487fc027
Andres Simoneau <andy20268@gmail.com>
2026-01-03 01:33:29 -0500

    s

--- skyrim.py
+++ skyrim.py
@@ -4,3 +4,4 @@ import sqlite3
 weapons = pd.read_csv('Skyrim_Weapons.csv')
 weapons_data = sqlite3.connect('skyrim.db')
 weapons.to_sql("weapons", weapons_data, if_exists="replace", index=False)
+weapons_data.close()
