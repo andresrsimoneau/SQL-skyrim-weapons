6aaeb38e82b97f2a094fdc5a5790b078f4f8283a
Andres Simoneau <andy20268@gmail.com>
2026-01-03 02:16:43 -0500

    s

--- skyrim.py
+++ skyrim.py
@@ -3,5 +3,18 @@ import sqlite3
 
 weapons = pd.read_csv('Skyrim_Weapons.csv') #pd.read_csv() function is used to read data from CSV files into a Pandas DataFrame
 weapons_data = sqlite3.connect('skyrim.db') #connecting to SQlite database with .db file
-weapons.to_sql("weapons", weapons_data, if_exists="replace", index=False) #putting panda dataframe into SQL
-weapons_data.close()
+weapons.to_sql("weaponsdata" , weapons_data, if_exists="replace", index=False) #putting panda dataframe into SQL
+                  #table     #SQlite connection
+weapons_sql = weapons_data.cursor() # allows us to execute SQL commands on the database
+
+BEST_DAMAGE_AND_CLASS = """
+SELECT damage, type
+FROM weaponsdata
+WHERE damage > 20
+ORDER BY damage DESC
+"""
+
+weapons_sql.execute(BEST_DAMAGE_AND_CLASS)
+joe_biden = weapons_sql.fetchall()
+for damage, type in joe_biden:
+        print(f"{damage} damage | {type} CLASS")
