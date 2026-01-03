a9e16d1c884d044808f6d615a5ddbed459de9c5e
Andres Simoneau <andy20268@gmail.com>
2026-01-03 01:58:20 -0500

    Update skyrim.py

--- skyrim.py
+++ skyrim.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import sqlite3
 
-weapons = pd.read_csv('Skyrim_Weapons.csv')
-weapons_data = sqlite3.connect('skyrim.db')
-weapons.to_sql("weapons", weapons_data, if_exists="replace", index=False)
+weapons = pd.read_csv('Skyrim_Weapons.csv') #pd.read_csv() function is used to read data from CSV files into a Pandas DataFrame
+weapons_data = sqlite3.connect('skyrim.db') #connecting to SQlite database with .db file
+weapons.to_sql("weapons", weapons_data, if_exists="replace", index=False) #putting panda dataframe into SQL
 weapons_data.close()
